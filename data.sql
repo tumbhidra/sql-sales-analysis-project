95316823975176a33ffad50599aa57469e4310cb
Rohith Naidu <112018257+tumbhidra@users.noreply.github.com>
2026-05-05 11:09:03 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,28 @@
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', '2023-01-10'),
+(2, 'Amit', 'Delhi', '2023-02-15'),
+(3, 'John', 'Mumbai', '2023-03-20'),
+(4, 'Rohith', 'Hospet', '2023-04-25'),
+(5, 'Chinni', 'Guntakal', '2023-05-05'),
+(6, 'Mithun', 'Indresham', '2023-06-10'),
+(7, 'Druvi', 'Patancheru', '2023-07-15');
+
+INSERT INTO products VALUES
+(101, 'Laptop', 'Electronics', 50000),
+(102, 'Phone', 'Electronics', 20000),
+(103, 'Table', 'Furniture', 10000),
+(104, 'Cable', 'Accessories', 2000),
+(105, 'Chair', 'Furniture', 1000),
+(106, 'Keyboard', 'Accessories', 3000),
+(107, 'Case', 'Accessories', 300);
+
+INSERT INTO orders VALUES
+(1, 1, 101, 2, '2024-01-01'),
+(2, 2, 102, 1, '2024-01-03'),
+(3, 1, 103, 3, '2024-01-05'),
+(4, 3, 104, 1, '2024-01-07'),
+(5, 4, 104, 2, '2024-01-10'),
+(6, 5, 105, 3, '2024-01-12'),
+(7, 6, 106, 4, '2024-01-15'),
+(8, 7, 106, 3, '2024-01-18'),
+(9, 7, 107, 5, '2024-01-20');
